6509b9dcc118a498b130e45a1c5890cb6a8d011a
Rafal Leszczynski <rafal@wikia-inc.com>
2015-09-27 20:45:38 +0200

    add messages for the rest of the page

--- src/messages.js
+++ src/messages.js
@@ -28,5 +28,87 @@ export default {
         '+48 24 362 00 48'
     ],
     fax: '+48 24 362 06 26',
-    email: 'biuro@tlumaczenia-agencja.pl'
+    email: 'biuro@tlumaczenia-agencja.pl',
+    contactUs: 'Napisz do nas',
+    offerSectionHeader: 'Oferta',
+    offer: [
+        {
+            title: 'Tłumaczenia pisemne',
+            description: 'Świadczymy usługi w zakresie tłumaczeń pisemnych zarówno przysięgłych, jak i nieprzysięgłych. Możemy przygotować dla Państwa m.in. tłumaczenia dokumentów smochodowych, tekstów specjalistycznych czy aktów prawnych.',
+            details: [
+                {
+                    title: 'Tłumaczenia przysięgłe'
+                },
+                {
+                    title: 'Tłumaczenia nieprzysięgłe'
+                }
+            ]
+        },
+        {
+            title: 'Tłumaczenia ustne',
+            description: 'Zajmujemy się obsługą tłumaczeniową konferencji, spotkań czy przemówień. Zespół naszych tłumaczy przełoży dla Państwa także rozmowy telefoniczne, a także będzie toważyszył podczas spotkań biznesowych. Dokonujemy również tłumaczeń przysięgłych m.in. w sądzie, w Urzędzie Stanu Cywilnego czy u notariusza.',
+            details: [
+                {
+                    title: 'Tłumaczenia przysięgłe i nieprzysięgłe'
+                }
+            ]
+        }
+    ],
+    languageSectionHeader: 'Języki',
+    languageSectionDescription: 'Wykonujemy tłumaczenia przysięgłe i nieprzysięgłe w następujących językach:',
+    languages: [
+        'angielski',
+        'białoruski',
+        'bułgarski',
+        'chiński',
+        'chorwacki',
+        'czeski',
+        'duński',
+        'estoński',
+        'francuski',
+        'grecki',
+        'hiszpański',
+        'japoński',
+        'litewski',
+        'łacina',
+        'łotewski',
+        'niderlandzki',
+        'niemiecki',
+        'norweski',
+        'portugalski',
+        'rosyjski',
+        'rumuński',
+        'serbski',
+        'słowacki',
+        'słoweński',
+        'szwedzki',
+        'turecki',
+        'ukraiński',
+        'węgierski',
+        'włoski'
+    ],
+    otherLanguagesDescription: 'Na Państwa życzenie możemy zorganizować także tłumaczenia uwzględniające inne języki',
+    prizesSectionHeader: 'Ceny',
+    prizes: [
+        {
+            description: 'Zapraszamy do skorzystania z naszego cennika, w którym znajdą Państwo orientacyjne ceny wszystkich oferowanych przez nas rodzajów tłumaczeń.',
+            details: [
+                {
+                    title: 'Przejdź do cennika'
+                }
+            ]
+        },
+        {
+            description: 'Oferujemy darmową wycenę Państwa dokumentów w 24 godziny. W odpowiedzi na wiadomość, otrzymają Państwo nie tylko dokładny koszt usługi tłumaczeniowej, ale także przybliżony termin realizacji.',
+            details: [
+                {
+                    title: 'Zamów darmową wycenę'
+                }
+            ]
+        }
+    ],
+    docsSectionHeader: 'Wzory',
+    docsSectionDescription: 'Przygotowaliśmy dla Państwa wzory dwujęzycznych umów kupna-sprzedaży samochodów z zagranicy.',
+    docsLanguageSelect: 'Wybierz język',
+    docsDownloadButton: 'Powierz'
 };
