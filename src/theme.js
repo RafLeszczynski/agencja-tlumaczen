0c2795cae80720c286171b76876ff42c35df16a8
Rafal Leszczynski <rafal@wikia-inc.com>
2015-09-27 20:44:54 +0200

    add shared theme properties

---
+++ src/theme.js
@@ -0,0 +1,8 @@
+export default {
+    brandingColor: '#421536',
+    secondaryColor: '#FFF9F5',
+    textAltColor: '#fff',
+    baselineFontSize: '14px',
+    baselineLineHeight: '20px',
+    sidePadding: '16px'
+}
