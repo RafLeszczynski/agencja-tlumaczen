ca1adeeff909a5e20dac92a6530aef7f8f0c3ffa
Rafal Leszczynski <rafal@wikia-inc.com>
2015-10-01 23:07:50 +0200

    add variation of branding color

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 export default {
-    brandingColor: '#421536',
+    brandingColor: 'rgba(66, 21, 54, 1)',
+    brandingLightColor: 'rgba(66, 21, 54, 0.7)',
     secondaryColor: '#FFF9F5',
     altColor: '#fff',
     baselineFontSize: '14px',
