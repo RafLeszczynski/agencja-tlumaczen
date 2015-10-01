9428b633fcb2d074bacac929945c01755db10da1
Rafal Leszczynski <rafal@wikia-inc.com>
2015-10-01 23:09:57 +0200

    add styling for different breakpoints to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,37 +39,36 @@ const styles = {
         letterSpacing: '7px'
     },
     title: {
-        fontSize: '40px',
+        fontSize: 40,
         fontWeight: 'bold',
         lineHeight: '60px',
-        margin: '0 0 16px',
+        margin: '0 0 42px',
 
         '@media (min-width: 768px)': {
-            fontSize: '48px',
+            fontSize: 48,
             letterSpacing: '9px'
         },
         '@media (min-width: 1024px)': {
-            fontSize: '64px',
+            fontSize: 64,
             letterSpacing: '12px',
             lineHeight: '81px'
         }
-
-
     },
     subTitle: {
-        fontSize: '18px',
+        fontSize: 18,
         fontWeight: 'normal',
         lineHeight: '30px',
-        margin: '0 0 45px',
+        margin: '0 0 72px',
 
         '@media (min-width: 768px)': {
             letterSpacing: '15px'
 
         },
         '@media (min-width: 1024px)': {
-            fontSize: '24px',
+            fontSize: 24,
             letterSpacing: '20px',
-            lineHeight: '27px'
+            lineHeight: '27px',
+            margin: '0 0 96px'
         }
     }
 };
