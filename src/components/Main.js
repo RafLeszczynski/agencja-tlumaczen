2023d0b072ec15841f5492ac3c0c05f3ae8d96cd
Rafal Leszczynski <rafal@wikia-inc.com>
2015-10-01 23:13:40 +0200

    create Contact component + refactor Main to use it

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,99 +1,65 @@
 import React from 'react';
+import Radium from 'radium';
 import Header from 'components/Header';
 import Section from 'components/Section';
 import OfficeAddress from 'components/OfficeAddress';
 import Button from 'components/Button';
 import SectionItem from 'components/SectionItem';
 import Languages from 'components/Languages';
+import Contact from 'components/Contact';
 import messages from 'messages';
 import theme from 'theme';
 
+@Radium
 class Main extends React.Component {
     render() {
-        let styles = {
-            color: theme.brandingColor,
-            fontFamily: 'Roboto, sans-serif',
-            fontSize: theme.baselineFontSize,
-            lineHeight: theme.baselineLineHeight
-        };
-
         return (
-            <div style={styles}>
-                <Header
-                    title={messages.pageTitle}
-                    subtitle={messages.pageSubtitle}
-                    actionButtonName={messages.showOfferDetails}
-                />
+            <div style={[styles]}>
+                <Header title={messages.pageTitle} subtitle={messages.pageSubtitle}
+                        actionButtonName={messages.showOfferDetails}/>
 
-                <Section
-                    title={messages.officeSectionHeader}
-                    id='office'
-                >
+                <Section title={messages.officeSectionHeader} id='office' flexLayout={true}>
                     {messages.offices.map((address) => {
                         let {...addressData} = address;
-                        return  (
-                            <OfficeAddress {...addressData} showLocation={messages.showLocation}
-                            />
-                        );
+                        return  <OfficeAddress {...addressData} showLocation={messages.showLocation}/>;
                     })}
                 </Section>
 
-                <Section
-                    title={messages.contactSectionHeader}
-                    id='contact'
-                    even={true}
-                >
-                    {messages.phones.map((number) => {
-                        return (
-                            <span>
-                                <a href={`tel:${number}`}>{number}</a><br/>
-                            </span>
-                        );
-                    })}
-                    <span>fax: {messages.fax}</span><br/>
-                    <span>{messages.email}</span><br/>
-                    <Button name={messages.contactUs} />
+                <Section title={messages.contactSectionHeader} id='contact' even={true}>
+                    <Contact {...messages.contactDetails} contactUs={messages.contactUs}/>
                 </Section>
 
-                <Section
-                    title={messages.offerSectionHeader}
-                    id='offer'
-                >
+                <Section title={messages.offerSectionHeader} id='offer'>
                     {messages.offer.map((item) => {
                         return <SectionItem item={item}/>;
                     })}
                 </Section>
 
-                <Section
-                    title={messages.languageSectionHeader}
-                    id='language'
-                    description={messages.languageSectionDescription}
-                    even={true}
-                >
+                <Section title={messages.languageSectionHeader} id='language'
+                         description={messages.languageSectionDescription} even={true}>
                     <Languages languages={messages.languages}/>
                     <p>{messages.otherLanguagesDescription}</p>
                 </Section>
 
-                <Section
-                    title={messages.prizesSectionHeader}
-                    id='prizes'
-                >
+                <Section title={messages.prizesSectionHeader} id='prizes'>
                     {messages.prizes.map((item) => {
                         return <SectionItem item={item}/>;
                     })}
                 </Section>
 
-                <Section
-                    title={messages.docsSectionHeader}
-                    id='docs'
-                    description={messages.docsSectionDescription}
-                    even={true}
-
-                    >
+                <Section title={messages.docsSectionHeader} id='docs' description={messages.docsSectionDescription}
+                    even={true}>
                 </Section>
             </div>
         );
     }
 }
+
+const styles = {
+    color: theme.brandingColor,
+    fontFamily: 'Roboto, sans-serif',
+    fontSize: theme.baselineFontSize,
+    lineHeight: theme.baselineLineHeight
+};
 
 export default Main;
--- src/messages.js
+++ src/messages.js
@@ -23,12 +23,14 @@ export default {
         }
     ],
     contactSectionHeader: 'Kontakt',
-    phones: [
-        '+48 693 348 565',
-        '+48 24 362 00 48'
-    ],
-    fax: '+48 24 362 06 26',
-    email: 'biuro@tlumaczenia-agencja.pl',
+    contactDetails: {
+        phones: [
+            '+48 693 348 565',
+            '+48 24 362 00 48'
+        ],
+        fax: 'fax: +48 24 362 06 26',
+        email: 'biuro@tlumaczenia-agencja.pl'
+    },
     contactUs: 'Napisz do nas',
     offerSectionHeader: 'Oferta',
     offer: [
