642ad27d99b4b2e833c12229ffff3b05ca2b937c
Rafal Leszczynski <rafal@wikia-inc.com>
2015-10-01 23:12:42 +0200

    add styles to Section Item Component

--- src/components/SectionItem.js
+++ src/components/SectionItem.js
@@ -1,29 +1,59 @@
 import React from 'react';
+import Radium from 'radium';
 import Button from 'components/Button';
+import theme from 'theme';
 
+@Radium
 class SectionItem extends React.Component {
     render() {
         let header,
             description;
 
         if (this.props.item.title) {
-            header = <h3>{this.props.item.title}</h3>;
+            header = <h3 style={[styles.header, styles.spacing]}>{this.props.item.title}</h3>;
         }
 
         if (this.props.item.description) {
-            description = <p>{this.props.item.description}</p>;
+            description = <p style={[styles.description, styles.spacing]}>{this.props.item.description}</p>;
         }
 
         return (
-            <article>
+            <div style={[styles.base]}>
                 {header}
                 {description}
                 {this.props.item.details.map((detail) => {
                     return <Button name={detail.title}/>
                 })}
-            </article>
+            </div>
         );
     }
 }
+
+const styles = {
+    base: {
+        margin: '0 auto',
+        textAlign: 'center',
+
+        '@media (min-width: 768px)': {
+            width: 540
+        }
+    },
+    description: {
+        color: theme.brandingLightColor
+    },
+    header: {
+        fontSize: 16,
+        fontWeight: 'normal',
+        textTransform: 'upperCase',
+
+        '@media (min-width: 768px)': {
+            fontSize: 18
+
+        }
+    },
+    spacing: {
+        marginBottom: 48
+    }
+};
 
 export default SectionItem;
