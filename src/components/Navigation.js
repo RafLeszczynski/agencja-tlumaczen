2ed8a24438340d9dfb274fe13ef1737bf9cf6199
Rafal Leszczynski <rafal@wikia-inc.com>
2015-11-15 13:57:56 +0100

    change navigation to stateless component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,30 +3,27 @@ require('../scss/components/navigation.scss');
 import React from 'react';
 import {Link} from 'react-scroll';
 
-export default class Navigation extends React.Component {
-	static propTypes = {
-		links: React.PropTypes.array.isRequired,
-		scrollOffset: React.PropTypes.number.isRequired,
-		scrollDuration: React.PropTypes.number.isRequired
-	};
+const Navigation = ({links, scrollDuration, scrollOffset}) => {
+	return (
+		<nav className='header__navigation'>
+			<ul className='header__navigation-list'>
+				{links.map((link) => {
+					return (
+						<li key={link.id} className='header__navigation-list__list-item'>
+							<Link to={link.id} spy={true} smooth={true} offset={scrollOffset}
+							      duration={scrollDuration}>{link.title}</Link>
+						</li>
+					)
+				})}
+			</ul>
+		</nav>
+	);
+};
 
-	render() {
-		let {links, scrollDuration, scrollOffset} = this.props;
+Navigation.propTypes = {
+	links: React.PropTypes.array.isRequired,
+	scrollOffset: React.PropTypes.number.isRequired,
+	scrollDuration: React.PropTypes.number.isRequired
+};
 
-		// TODO: Move home link to navigation
-		return (
-			<nav className='header__navigation'>
-				<ul className='header__navigation-list'>
-					{links.map((link) => {
-						return (
-							<li key={link.id} className='header__navigation-list__list-item'>
-								<Link to={link.id} spy={true} smooth={true} offset={scrollOffset}
-								      duration={scrollDuration}>{link.title}</Link>
-							</li>
-						)
-					})}
-				</ul>
-			</nav>
-		);
-	}
-}
+export default Navigation;
