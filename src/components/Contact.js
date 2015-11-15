536f3a4c302bab4bb7d1ec0bcef9471b1b23b4c7
Rafal Leszczynski <rafal@wikia-inc.com>
2015-11-15 14:11:15 +0100

    change Contact to stateless component and move styles to scss

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,56 +1,30 @@
+require('../scss/components/contact.scss');
+
 import React from 'react';
-import Radium from 'radium';
 import Button from 'components/Button';
 
-@Radium
-class Contact extends React.Component {
-    static propTypes = {
-        contactUs: React.PropTypes.string.isRequired,
-        email: React.PropTypes.string.isRequired,
-        fax: React.PropTypes.string.isRequired,
-        phones: React.PropTypes.array.isRequired
-    };
+const Contact = ({contactUs, email, fax, phones}) => {
+	return (
+		<div className="contact">
+			{phones.map((number, index) => {
+				return (
+					<span key={index} className="contact__phone">
+						<a href={`tel:${number}`}>{number}</a>
+					</span>
+				);
+			})}
+			<span className="contact__fax">{fax}</span>
+			<span className="contact__email">{email}</span>
+			<Button name={contactUs}/>
+		</div>
+	);
+};
 
-    render() {
-        let {contactUs, email, fax, phones } = this.props;
-        return (
-            <div style={[styles.base]}>
-                {phones.map((number) => {
-                    return (
-                        <span style={[styles.block, styles.phoneSpacing]}>
-                            <a href={`tel:${number}`}>{number}</a>
-                        </span>
-                    );
-                })}
-                <span style={[styles.block, styles.faxSpacing]}>{fax}</span>
-                <span style={[styles.block, styles.emailSpacing]}>{email}</span>
-                <Button name={contactUs} />
-            </div>
-        );
-    }
-}
-
-const styles = {
-    base: {
-        fontSize: 16,
-        textAlign: 'center'
-    },
-    block: {
-        display: 'block'
-    },
-    phoneSpacing: {
-        marginBottom: 16
-    },
-    faxSpacing: {
-        marginBottom: 42
-    },
-    emailSpacing: {
-        marginBottom: 42,
-
-        '@media (min-width: 768px)': {
-            marginBottom: 72
-        }
-    }
+Contact.propTypes = {
+	contactUs: React.PropTypes.string.isRequired,
+	email: React.PropTypes.string.isRequired,
+	fax: React.PropTypes.string.isRequired,
+	phones: React.PropTypes.array.isRequired
 };
 
 export default Contact;
