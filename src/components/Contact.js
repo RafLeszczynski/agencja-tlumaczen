38e42bc2d50dc409cf9ceba55d221011301798f6
Rafal Leszczynski <rafal@wikia-inc.com>
2015-11-22 17:13:03 +0100

    show contact form in modal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ require('../scss/components/contact.scss');
 import React from 'react';
 import Button from 'components/Button';
 
-const Contact = ({contactUs, email, fax, phones}) => {
+const Contact = ({contactUs, email, fax, phones, showModal}) => {
 	return (
 		<div className="contact">
 			{phones.map((number, index) => {
@@ -15,16 +15,21 @@ const Contact = ({contactUs, email, fax, phones}) => {
 			})}
 			<span className="contact__fax">{fax}</span>
 			<span className="contact__email">{email}</span>
-			<Button name={contactUs}/>
+			<Button name={contactUs} action={showContactForm.bind(this, showModal)} />
 		</div>
 	);
 };
+
+function showContactForm(showModal) {
+	showModal('contact');
+}
 
 Contact.propTypes = {
 	contactUs: React.PropTypes.string.isRequired,
 	email: React.PropTypes.string.isRequired,
 	fax: React.PropTypes.string.isRequired,
-	phones: React.PropTypes.array.isRequired
+	phones: React.PropTypes.array.isRequired,
+	showModal: React.PropTypes.func.isRequired
 };
 
 export default Contact;
