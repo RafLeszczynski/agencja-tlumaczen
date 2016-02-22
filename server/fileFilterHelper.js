93b6622cfdd73acc9c1f3b19c3545f3c7a2059f9
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-22 18:58:06 +0100

    ATLUMACZEN-32, fix linter issues in sever side code

--- server/fileFilterHelper.js
+++ server/fileFilterHelper.js
@@ -1,19 +1,28 @@
 export const supportedTypes = [
-	"application/pdf",
-	"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-	"application/msword",
-	"application/vnd.oasis.opendocument.text",
-	"image/png",
-	"application/rtf",
-	"image/jpeg"
+	'application/pdf',
+	'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+	'application/msword',
+	'application/vnd.oasis.opendocument.text',
+	'image/png',
+	'application/rtf',
+	'image/jpeg'
 ];
 
-export default function (req, file, cb) {
-	const mimetype = file.mimetype;
+/**
+ * @desc checks if given file has supported mime type
+ * @param {Object} req - HTTP request object
+ * @param {Object} file - file data object
+ * @param {Function} cb - callback function
+ * @returns {void}
+ */
+export default (req, file, cb) => {
+	const mimeType = file.mimetype;
+	const negativeIndex = -1;
 
-	if (supportedTypes.indexOf(mimetype) === -1) {
-		cb(new Error(`File format ${mimetype} not supported`));
-	} else {
-		cb(null, true);
+	if (supportedTypes.indexOf(mimeType) === negativeIndex) {
+		cb(new Error(`File format ${mimeType} not supported`));
+		return;
 	}
-}
+
+	cb(null, true);
+};
