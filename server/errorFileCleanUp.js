b7cfbb8e8fb930c4932fffd7e64215199d222692
Rafal Leszczynski <rafal@wikia-inc.com>
2016-03-28 21:43:44 +0200

    ATLUMACZEN-9, add erro file cleanup middleware

---
+++ server/errorFileCleanUp.js
@@ -0,0 +1,17 @@
+import uploadsCleanup from './uploadsCleanupHelper';
+
+/**
+ * @desc cleaup files on sendMessage error
+ * @param {Object} error - node error Object
+ * @param {Object} request - express request object
+ * @param {Object} response - express response object
+ * @param {Function} next - express next callback
+ */
+export default (error, request, response, next) => {
+	const files = request.files;
+
+	if (error && files)
+	uploadsCleanup(files);
+
+	next(error);
+};
