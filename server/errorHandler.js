8bc65eada867bbd675ae9924becac9a8a0be87d4
Rafal Leszczynski <rafal@wikia-inc.com>
2016-03-28 21:43:06 +0200

    ATLUMACZEN-9, add error handler middleware

---
+++ server/errorHandler.js
@@ -0,0 +1,25 @@
+const defaultStatusCode = 500;
+
+/**
+ *@desc creates error response JSON object
+ * @param {Object} error - node error object
+ * @returns {Object} - error response JSON object
+ */
+function createErrorResponseJsonObject(error) {
+	return {
+		message: error.message,
+		errorCode: error.errorCode,
+		details: error.details
+	}
+}
+
+/**
+ * @desc custom error handler express js middleware
+ * @param {Object} error - node error Object
+ * @param {Object} request - express request object
+ * @param {Object} response - express response object
+ * @param {Function} next - express next callback
+ */
+export default (error, request, response, next) => {
+	response.status(error.statusCode || defaultStatusCode).json(createErrorResponseJsonObject(error));
+};
