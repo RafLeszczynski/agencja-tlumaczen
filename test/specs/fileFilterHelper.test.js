135be735d2bafee67c1da991e3169e1a339aa5dc
Rafal Leszczynski <rafal@wikia-inc.com>
2016-04-10 20:07:38 +0200

    ATLUMACZEN-9, fix unsupported file type error generation

--- test/specs/fileFilterHelper.test.js
+++ test/specs/fileFilterHelper.test.js
@@ -1,9 +1,8 @@
-import fileFilter, {supportedTypes} from '../../server/fileFilterHelper';
+import fileFilter, {supportedTypes, createUnsupportedTypeErrorObject} from '../../server/fileFilterHelper';
 
 describe('Mail Sender tests', () => {
-	const requestMock = {};
-
 	it('accepts supported file types', () => {
+		const requestMock = {};
 		const files = supportedTypes.map(mimetype => {
 			return {mimetype};
 		});
@@ -19,17 +18,40 @@ describe('Mail Sender tests', () => {
 		});
 	});
 
-	it('throws error for not supported file types', () => {
+	it('adds error details to request object for unsupported file types', () => {
+		const requestMock = {};
+		const unsupportedFileTypeErrorsKey = 'unsupportedFileTypeErrors';
 		const cb = jasmine.createSpy('cb');
 		const unsupportedType = 'application/unsupportedType';
-		const error = new Error(`File format ${unsupportedType} not supported`);
-		const unsupportedFileMock = {
-			mimetype: unsupportedType
-		};
+		const unsupportedFiles = [
+			{
+				originalname: 'lorem.test',
+				mimetype: unsupportedType
+			},
+			{
+				originalname: 'ipsum.test',
+				mimetype: unsupportedType
+			}
+		];
 
-		fileFilter(requestMock, unsupportedFileMock, cb);
+		unsupportedFiles.forEach(file => {
+			fileFilter(requestMock, file, cb);
+		});
 
-		expect(cb).toHaveBeenCalled();
-		expect(cb).toHaveBeenCalledWith(error);
+		expect(cb).toHaveBeenCalledTimes(2);
+		expect(cb).toHaveBeenCalledWith(null, false);
+		expect(requestMock.hasOwnProperty(unsupportedFileTypeErrorsKey)).toBe(true);
+		expect(Array.isArray(requestMock[unsupportedFileTypeErrorsKey])).toBe(true);
+		expect(requestMock[unsupportedFileTypeErrorsKey].length).toBe(2);
+	});
+
+	it('creates correct unsupported file type error details object', () => {
+		const mimeType = 'test/test';
+		const fileName = 'tes.jpg';
+		const errorDetails = createUnsupportedTypeErrorObject(fileName, mimeType);
+
+		expect(errorDetails.message).toBe('File format not supported');
+		expect(errorDetails.fileName).toBe(fileName);
+		expect(errorDetails.mimeType).toBe(mimeType);
 	});
 });
