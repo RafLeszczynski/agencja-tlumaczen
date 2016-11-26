37dbf41ad24bcea6192275bef84b085732f147fb
Rafal Leszczynski <raf.leszcz@gmail.com>
2016-11-26 19:43:25 +0100

    ATLUMACZEN-22, fix linter issues

--- render-app-to-file.js
+++ render-app-to-file.js
@@ -1,23 +1,27 @@
 #! /usr/bin/env node
 
 import fs from 'fs';
-import React from 'react'
+import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import Application from './app/components/Application/Application';
 
 const path = `${__dirname}/build/index.html`;
 
-fs.readFile(path, 'utf8', (err,data) => {
-  if (err) {
-    return console.log(err);
-  }
+fs.readFile(path, 'utf8', (err, data) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
 
-  fs.writeFile(
-    path,
-    data.replace('{{content}}', ReactDOMServer.renderToString(<Application/>)),
-    'utf8',
-    (err) => {
-      if (err) return console.log(err);
-    }
-  );
+	fs.writeFile(
+		path,
+		data.replace('{{content}}', ReactDOMServer.renderToString(<Application/>)),
+		'utf8',
+		error => {
+			if (error) {
+				console.log(error);
+				return;
+			}
+		}
+	);
 });
