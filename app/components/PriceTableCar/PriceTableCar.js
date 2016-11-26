034e81700f4ea7b63443609e918632b0267a7390
Rafal Leszczynski <raf.leszcz@gmail.com>
2016-11-26 14:33:00 +0100

    ATLUMACZEN-71, make expand langs chevron clickable

--- app/components/PriceTableCar/PriceTableCar.js
+++ app/components/PriceTableCar/PriceTableCar.js
@@ -254,7 +254,7 @@ export default class PriceTableCar extends React.Component {
 				<div className='table-expander'>
 					<strong onClick={this._toggleExpand.bind(this)}>{this.state.expended ? 'mniej ' : 'więcej '}
 						języków</strong>
-					<svg className={arrowClassName} viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+					<svg onClick={this._toggleExpand.bind(this)} className={arrowClassName} viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
 						<path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'/>
 						<path d='M0 0h24v24H0z' fill='none'/>
 					</svg>
