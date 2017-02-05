e70f4763265e148eeaa5eccf8967a1ba6ba2ede1
Rafal Leszczynski <raf.leszcz@gmail.com>
2017-02-05 14:02:06 +0100

    small styling and typo fixes

--- app/components/Docs/Docs.messages.js
+++ app/components/Docs/Docs.messages.js
@@ -1,17 +1,53 @@
 export const docsSectionDescription = 'Przygotowaliśmy dla Państwa wzory dwujęzycznych umów kupna-sprzedaży' +
 	' samochodów z zagranicy.';
-export const docsDocSelect = 'Wybierz dokument';
+export const docsDocSelect = 'Wybierz';
 export const docs = [
 	{
-		value: '1.pdf',
-		label: 'Temp 1'
+		value: 'docs/umowa-polsko-angielska.pdf',
+		label: 'polsko - angielska'
 	},
 	{
-		value: '2.pdf',
-		label: 'Temp 2'
+		value: 'docs/umowa-polsko-belgijska.pdf',
+		label: 'polsko - belgijska'
 	},
 	{
-		value: '3.pdf',
-		label: 'Temp 3'
-	}
+		value: 'docs/umowa-polsko-czeska.pdf',
+		label: 'polsko - czeska'
+	},
+	{
+		value: 'docs/umowa-polsko-dunska.pdf',
+		label: 'polsko - duńska'
+	},
+	{
+		value: 'docs/umowa-polsko-francuska.pdf',
+		label: 'polsko - francuska'
+	},
+	{
+		value: 'docs/umowa-polsko-hiszpanska.pdf',
+		label: 'polsko - hiszpańska'
+	},
+	{
+		value: 'docs/umowa-polsko-holenderska.pdf',
+		label: 'polsko - holenderska'
+	},
+	{
+		value: 'docs/umowa-polsko-litewska.pdf',
+		label: 'polsko - litewska'
+	},
+	{
+		value: 'docs/umowa-polsko-niemiecka.pdf',
+		label: 'polsko - niemiecka'
+	},
+	{
+		value: 'docs/umowa-polsko-rosyjska.pdf',
+		label: 'polsko - rosyjska'
+	},
+	{
+		value: 'docs/umowa-polsko-szwedzka.pdf',
+		label: 'polsko - szwedzka'
+	},
+	{
+		value: 'docs/umowa-polsko-wloska.pdf',
+		label: 'polsko - włoska'
+	},
 ];
